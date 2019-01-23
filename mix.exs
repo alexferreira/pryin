8ddf9889285073514e986c331917627841c36639
Alex Ferreira <alex@dsol.com.br>
2019-01-23 11:00:15 -0200

    update version of recon

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule PryIn.Mixfile do
       {:exprotobuf, "~> 1.2"},
       {:ex_doc, "~> 0.15", only: :dev},
       {:credo, "~> 0.8", only: :dev},
-      {:recon, "~> 2.4"},
+      {:recon, "~> 2.3.2"},
       {:ex_machina, "~> 2.0", only: :test}
     ]
   end
